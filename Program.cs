22ca1775f9b91d7b323bc7cb7439031fd8844265
troubleman96 <itslugenge96@gmail.com>
2024-11-09 18:33:51 +0300

    uno

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program{
+
+    static void Main(string [] arg){
+
+        Console.Write("Enter your age: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        if(age >= 18 && age <= 25){
+            Console.WriteLine("Your age is between 18 and 25");
+        }
+        else if(age >= 26 || age <= 50){
+            Console.WriteLine("26 or 50");
+        }
+
+    }
+}
